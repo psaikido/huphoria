5f1027a847bcfa56ac7d25298dc95994eddd2874
Hugh Carroll <hughie@carrollonline.co.uk>
2023-11-26 13:42:07 +0000

    Add a flat file for data and a means of getting it.

--- src/menu_list.c
+++ src/menu_list.c
@@ -1,7 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include "../include/huf_menu_funcs.h" 
 
+FILE* getFile() {
+	char filename[100];
+	strcpy (filename, getenv("HOME"));
+	strcat (filename, "/code/huphoria/data/data.txt");
+
+	FILE *f = NULL;
+	
+	errno = 0;
+	f = fopen(filename, "r");
+
+	if (f == NULL) {
+		fprintf(stderr, "Error opening %s for reading, errno: %d\n", filename, errno);
+		exit(1);
+	} else {
+		return f;
+	}
+}
+
 void huf_list() {
-	printf("the list\n");
+	printf("::huphoria::\n");
+
+	FILE *f = getFile();
+
+	// int i = 0;
+	char line[255];
+	// char *lines;
+
+    while (fgets(line, sizeof(line), f)) {
+		printf("%s", line);
+    }
+
+	fclose(f);
 }
 
 void huf_save() {
